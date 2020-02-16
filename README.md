1f4ff1e28e847e68bb84bbec746495e1e35de79c
haomaoxiang <haomaoxiang@126.com>
2020-02-16 23:17:26 +0800

    feat(项目): 项目初始化

---
+++ README.md
@@ -0,0 +1,22 @@
+# 简介
+
+spring-boot-init是一个基于SpringBoot2.0，集成了常用开发组件的后台开发框架。
+
+此项目借鉴于[spring-boot-plus](https://gitee.com/geekidea/spring-boot-plus)
+
+# 主要特性
+
+1. 集成了常用组件，AOP日志等
+2. 集成MybatisPlus，自动生产dao、mapper、service以及controller代码
+3. 集成druid连接池，JDBC性能和慢查询检测
+4. 集成swagger2，自动生成api文档
+5. 集成spring boot admin，实时检测项目运行情况
+6. 持续更新
+
+# 项目环境
+
+# 技术选型
+
+# 项目目录
+
+# 项目部署
---
+++ src/main/java/me/mervyn/SpringBootInitApplication.java
@@ -0,0 +1,16 @@
+package me.mervyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author haomaoxiang@126.com
+ */
+@SpringBootApplication
+public class SpringBootInitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootInitApplication.class, args);
+    }
+
+}
